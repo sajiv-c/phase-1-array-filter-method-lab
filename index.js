c82e3dc262aacca709e1f88d52b51d0ba5f9096d
Sajiv Chandrasekaran <sajivc88@gmail.com>
2023-10-06 16:24:31 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution here
+const drivers2 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' 
+    },
+    {
+      name: 'Sammy',
+      hometown: 'New York' 
+    },
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' 
+    },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' 
+    },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' 
+    }
+];
+
+const drivers1 = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(arr, nameInput) {
+    return arr.filter((el) => el.toLowerCase() === nameInput.toLowerCase());
+    
+        // if(nameValues == nameInput) {
+        //     return nameValues.name;
+        //     console.log(nameValues.value);
+        // }
+        // else {
+        //     return([0]);
+        // }
+}
+
+function fuzzyMatch(arr, letters) {
+    return arr.filter((el) => el.slice(0, letters.length) === letters);
+}
+
+function matchName(arr, name) {
+    return arr.filter((el) => el['name'] === name);
+}
